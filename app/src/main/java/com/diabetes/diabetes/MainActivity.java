bf952c911fca9710541fed871ab93c747e40fbcd
Mikhail Burnaev <80346456+Cooperos@users.noreply.github.com>
2023-04-16 03:02:51 +0300

    RAW
    Added button, that redirects to profile page (activity_profile.xml)
    
     Can be replaced with an action bar soon

--- app/src/main/java/com/diabetes/diabetes/MainActivity.java
+++ app/src/main/java/com/diabetes/diabetes/MainActivity.java
@@ -2,14 +2,32 @@ package com.diabetes.diabetes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
+    Button switchToProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        switchToProfile = findViewById(R.id.button_toProfile);
+        switchToProfile.setOnClickListener(view -> switchActivities());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    private void switchActivities(){
+        Intent switchActivityIntent = new Intent(this, Profile.class);
+        switchActivityIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(switchActivityIntent);
     }
 }
